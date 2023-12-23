cdfed7478cd2e9c8127bdfa7022711f8e7153d1e
Aman kohare <amankohare@gmail.com>
2023-12-23 18:44:52 +0530

    added

--- src/components/index.js
+++ src/components/index.js
@@ -8,4 +8,4 @@ export { default as SearchBar } from './SearchBar';
 export { default as VideoDetail } from './VideoDetail';
 export { default as SearchFeed } from './SearchFeed';
 export { default as Videos } from './Videos';
-export { default as Sidebar } from './Sidebar';
+export { default as Sidebar } from './SideBar';
